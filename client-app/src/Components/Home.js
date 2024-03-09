26f55c649b4f5dbfd783d83bbe345ba1ddd87769
SiriChandanaGarimella <garimellasirichandana@gmail.com>
2024-03-08 18:40:08 -0600

    changes to about page content

--- client-app/src/Components/Home.js
+++ client-app/src/Components/Home.js
@@ -6,41 +6,11 @@ export default function Home() {
     <div className="bg-dark text-white p-5">
       <div className="row">
         <div className="col-md-6">
-          <h1 className="display-3 fw-bold">ST. LOUIS BWORKS SHOWS LOCAL KIDS WHAT’S POSSIBLE.</h1>
+          <h1 className="display-3 fw-bold">MATERIAL DONOR MUTUTAL ASSIST.</h1>
           <p className="mt-4 fs-5">
-            Our programs build confidence and character through educational programs rooted in teamwork and
-            responsibility. We accept donations of both adult and children’s sized bicycles.
+            This application is for maintaining the donor details, to track the donations and to send the donors
+            timely emails. This encoragues the engagement with donors.
           </p>
-          <div className="mt-4">
-            <h2 className="fs-4 fw-bold">Quick Links:</h2>
-            <ul className="list-unstyled mt-2">
-              <li>
-                <a className="text-decoration-none text-info" href="#">
-                  Upcoming Classes & Registration.
-                </a>
-              </li>
-              <li>
-                <a className="text-decoration-none text-info" href="#">
-                  Shop for Bicycles.
-                </a>
-              </li>
-              <li>
-                <a className="text-decoration-none text-info" href="#">
-                  Request a Donation Acknowledgement.
-                </a>
-              </li>
-              <li>
-                <a className="text-decoration-none text-info" href="#">
-                  Remote Drop Off Locations.
-                </a>
-              </li>
-              <li>
-                <a className="text-decoration-none text-info" href="#">
-                  Our Hours of Operation.
-                </a>
-              </li>
-            </ul>
-          </div>
         </div>
         <div className="col-md-6 position-relative">
           <img
