80113e6ed2b714ae661a81e1909de90bbd8ef73c
Joseph Hansen <joseph.hansen@slu.edu>
2024-09-13 21:08:13 -0500

    Enums properly defined and nothing broke

---
+++ client-app/constants/Enums.js
@@ -0,0 +1,9 @@
+const ItemStatus = Object.freeze ({
+    DONATED: 'Donated',
+    IN_STORAGE: 'In storage facility',
+    REFURBISHED: 'Refurbished',
+    SOLD: 'Item sold',
+    RECEIVED: 'Received'
+});
+
+export default ItemStatus;
