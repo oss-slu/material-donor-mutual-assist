ccbb2cae4cbf68bf463be5258b806aabbcc1ba37
miabu-pashh <mdmaibubpasha@gmail.com>
2024-10-21 15:10:25 -0500

    migration file

---
+++ server/prisma/migrations/20241021194130_updated_noimage/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `imageUrls` on the `DonatedItem` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "DonatedItem" DROP COLUMN "imageUrls";
