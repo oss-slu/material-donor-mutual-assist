87c109904d689f394418d127c5e3298340c6d990
miabu-pashh <mdmaibubpasha@gmail.com>
2024-10-27 14:06:22 -0500

    Refactor Enums module and package.json dependencies

---
+++ client-app/src/constants/Enums.tsx
@@ -0,0 +1,10 @@
+declare module 'constants/Enums' {
+    const ItemStatus: {
+        DONATED: string;
+        IN_STORAGE: string;
+        REFURBISHED: string;
+        RECEIVED: string;
+        SOLD: string;
+    };
+    export default ItemStatus;
+}
