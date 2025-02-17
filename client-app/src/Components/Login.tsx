692c2732a421051387dd28bf0a5a5110fdc66506
Joseph Hansen <joseph.hansen@slu.edu>
2025-02-17 16:24:06 -0600

    Updated Login to typescript

--- client-app/src/Components/Login.tsx
+++ client-app/src/Components/Login.tsx
@@ -1,20 +1,29 @@
-import React, { useState } from 'react';
+import React, { useState, ChangeEvent, FormEvent } from 'react';
 import { Link } from 'react-router-dom';
 
-const Login = props => {
-    const [credentials, setCredentials] = useState({ email: '', password: '' });
-    const [captcha, setCaptcha] = useState('');
-    const [captchaValue, setCaptchaValue] = useState('');
-    const [errorMessage, setErrorMessage] = useState('');
+interface Credentials {
+    email: string;
+    password: string;
+}
 
-    localStorage.setItem('isLogged', false);
+const Login: React.FC = () => {
+    const [credentials, setCredentials] = useState<Credentials>({
+        email: '',
+        password: '',
+    });
+    const [captcha, setCaptcha] = useState<string>('');
+    const [captchaValue, setCaptchaValue] = useState<string>('');
+    const [errorMessage, setErrorMessage] = useState<string>('');
+    const [showPopup, setShowPopup] = useState(false);
 
-    const generateCaptcha = () => {
+    localStorage.setItem('isLogged', 'false');
+
+    const generateCaptcha = (): void => {
         const randomCaptcha = Math.random().toString(36).substring(7);
         setCaptcha(randomCaptcha);
     };
 
-    const handleSubmit = async e => {
+    const handleSubmit = (e: FormEvent<HTMLFormElement>): void => {
         e.preventDefault();
 
         // Validate CAPTCHA
@@ -22,33 +31,26 @@ const Login = props => {
             setErrorMessage('Incorrect CAPTCHA. Please try again.');
             return;
         }
-        // const response = await fetch("http://localhost:5000/api/auth/login", {
-        //     method: 'POST',
-        //     headers: {
-        //         'Content-Type': 'application/json'
-        //     },
-        //     body: JSON.stringify({ email: credentials.email, password: credentials.password })
-        // });
-        // const json = await response.json();
 
-        // if (json.success) {
-        //     localStorage.setItem('token', json.authtoken);
-        //     localStorage.setItem('name', json.name);
-        localStorage.setItem('isLogged', true);
+        // Simulated login success
+        localStorage.setItem('isLogged', 'true');
         window.location.href = '/Donations';
-        // } else {
-        //     setErrorMessage("Invalid credentials");
-        // }
         alert('Login Success');
+        setShowPopup(true);
+
+        // Hide the pop-up after 3 seconds
+        setTimeout(() => {
+            setShowPopup(false);
+        }, 5000);
 
         console.log('in login page ', localStorage.getItem('isLogged'));
     };
 
-    const onChange = e => {
-        setCredentials({ ...credentials, [e.target.name]: e.target.value });
+    const onChange = (e: ChangeEvent<HTMLInputElement>): void => {
+        setCredentials(prev => ({ ...prev, [e.target.name]: e.target.value }));
     };
 
-    const handleCaptchaChange = e => {
+    const handleCaptchaChange = (e: ChangeEvent<HTMLInputElement>): void => {
         setCaptchaValue(e.target.value);
     };
 
@@ -113,7 +115,6 @@ const Login = props => {
                 <Link to="/forgot-password" className="btn btn-link">
                     Forgot Password?
                 </Link>{' '}
-                {/* Link to Forgot Password page */}
                 <button
                     type="button"
                     className="btn btn-secondary"
