f5fe3fbe2669009057e73b982b46faca654f69b7
Joseph Hansen <joseph.hansen@slu.edu>
2025-03-03 15:18:25 -0600

    Password correctly changes

--- server/src/routes/passwordResetRoutes.ts
+++ server/src/routes/passwordResetRoutes.ts
@@ -3,7 +3,8 @@ import prisma from '../prismaClient'; // Import Prisma client
 import { donorValidator } from '../validators/donorValidator';
 import { body, validationResult } from 'express-validator';
 import { sendPasswordReset } from '../services/emailService';
-import jwt from 'jsonwebtoken';
+import jwt, { JwtPayload } from 'jsonwebtoken';
+import bcrypt from 'bcryptjs';
 
 const router = Router();
 const JWT_SECRET = process.env.JWT_SECRET; // Use secret from .env
@@ -18,37 +19,70 @@ router.post(
     ],
     async (req: Request, res: Response) => {
         const { email } = req.body;
-    
-        if (!email) {
-            return res.status(400).json({ message: 'Email is required' });
-        }
-    
-    
+
         try {
-            await sendPasswordReset(email, "1");
-            res.json({ message: 'Password reset email sent' });
+            // Find user in the database
+            const user = await prisma.user.findUnique({ where: { email } });
+            if (!user) {
+                return res.status(401).json({
+                    message:
+                        'Invalid email, please register to proceed with login.',
+                });
+            }
+
+            // Generate JWT token and it expires in 1hr.
+            const token = jwt.sign(
+                { userId: user.id, email: user.email },
+                JWT_SECRET,
+                { expiresIn: '1h' },
+            );
+
+            try {
+                await sendPasswordReset(email, token);
+                res.json({ message: 'Password reset email sent' });
+            } catch (error) {
+                console.error('Error sending reset email:', error);
+                res.status(500).json({ message: 'Error sending email' });
+            }
         } catch (error) {
-            console.error('Error sending reset email:', error);
-            res.status(500).json({ message: 'Error sending email' });
+            console.error('Login Error:', error);
+            return res.status(500).json({ message: 'Internal server error' });
         }
     }
 );
 
-// router.post('/forgotpassword', async (req, res) => {
-//     const { email } = req.body;
+router.post(
+    '/resetpassword',
+    async (req: Request, res: Response) => {
+        const { token, password } = req.body;
 
-//     if (!email) {
-//         return res.status(400).json({ message: 'Email is required' });
-//     }
+        let decoded:JwtPayload;
+        try {
+            decoded = jwt.verify(token, JWT_SECRET) as JwtPayload; // Decode and verify the token
+        } catch (error) {
+            return res.status(401).json({ message: 'Invalid or expired token' });
+        }
 
+        const userId = decoded.userId;
 
-//     try {
-//         await sendPasswordReset(email, "1");
-//         res.json({ message: 'Password reset email sent' });
-//     } catch (error) {
-//         console.error('Error sending reset email:', error);
-//         res.status(500).json({ message: 'Error sending email' });
-//     }
-// });
+        try {
+            // Hash the password
+            const newHashedPassword = await bcrypt.hash(password, 10);
+
+            // Store user in database
+            const updatedUser = await prisma.user.update({
+                where: { id: userId },
+                data: { password: newHashedPassword }, // The new hashed password
+            });
+
+            return res.status(201).json({
+                message: 'Password changed successfully',
+            });
+        } catch (error) {
+            console.error('Login Error:', error);
+            return res.status(500).json({ message: 'Internal server error' });
+        }
+    }
+);
 
 export default router;
