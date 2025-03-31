2214a867bc1e0deab0390541a9e6095eb53e0eb3
Joseph Hansen <joseph.hansen@slu.edu>
2025-03-31 03:34:53 -0500

    Removed comments

--- server/src/routes/routeProtection.ts
+++ server/src/routes/routeProtection.ts
@@ -5,40 +5,31 @@ const JWT_SECRET = process.env.JWT_SECRET;
 if (!JWT_SECRET) {
     throw new Error('JWT_SECRET is not set in .env file!');
 }
-//console.log(process.env.JWT_SECRET);
 
 export const authenticateUser = async (
     req: Request,
     res: Response,
     adminPerm: Boolean,
 ) => {
-    // const authHeader = req.headers.authorization;
-    //console.log('Checkpoint 2.1');
     try {
         const authHeader = req.headers.authorization;
-        //console.log('Checkpoint 3');
         if (!authHeader) {
-            //throw new Error("Access denied: Not logged in");
-            //console.log('Checkpoint 4');
             res.status(401).json({ message: 'Access denied: Not logged in' });
             return false;
         }
-        //console.log('Checkpoint 5');
         const decoded = jwt.verify(authHeader, JWT_SECRET) as { role: string };
         if (decoded.role != 'ADMIN' && adminPerm) {
-            res.status(401).json({ message: 'Access denied: Insufficient permissions' });
+            res.status(401).json({
+                message: 'Access denied: Insufficient permissions',
+            });
             return false;
-            //return res
-            //    .status(401)
-            //    .json({ message: 'Access denied: Insufficient permissions' });
-            //throw new Error("Access denied: Insufficient permission");
         } else {
+            res.status(200).json;
             return true;
-            //return res.status(200).json;
         }
     } catch (error) {
         console.error('Permission error:', error);
+        res.status(500).json({ message: 'Internal server error' });
         return false;
-        //return res.status(500).json({ message: 'Internal server error' });
     }
 };
