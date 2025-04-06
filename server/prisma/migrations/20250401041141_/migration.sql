459e4c94cb36d9cb74c6be6e60f2cb43b2f1b9f0
alar12 <adithyaavuthu@gmail.com>
2025-04-05 19:04:48 -0500

    added first login flag to migrations

---
+++ server/prisma/migrations/20250401041141_/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE "User" ADD COLUMN     "firstLogin" BOOLEAN NOT NULL DEFAULT true,
+ADD COLUMN     "resetToken" TEXT,
+ADD COLUMN     "resetTokenExpiry" TIMESTAMP(3),
+ALTER COLUMN "role" DROP NOT NULL;
