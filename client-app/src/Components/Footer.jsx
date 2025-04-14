4806f4620369b9b7659802cc71ff4bb87989e898
atiqullah <atiqullah.asghari@slu.edu>
2025-04-14 14:18:54 -0500

    updates

---
+++ client-app/src/Components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      © 2025 St. Louis BWorks. 2414 Menard Street, St. Louis, MO 63104, (314) 664-0828
+    </footer>
+  );
+};
+
+export default Footer;
